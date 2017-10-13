036e719a2dd519ca50faf503bba798abb52ba45e
Vincent Shen <chengwei.shen@EF.com>
2017-10-13 13:04:33 +0800

    update prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
-  apiServerUrl: ''
+  apiServerUrl: 'http://differencesapi.azurewebsites.net/',
+  authSettings: {
+    tenantName: 'DifferencesB2CTenant.onmicrosoft.com',
+    clientId: '8c73e2ca-6062-4793-ae08-42723068a8e2',
+    scopes: ['https://DifferencesB2CTenant.onmicrosoft.com/api/read openid offline_access'],
+    policyName: 'B2C_1_Sign',
+  }
 };
