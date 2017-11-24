2e1498fbbb155728524faf442871b91ca4f4b2db
Vincent Shen <chengwei.shen@EF.com>
2017-11-24 16:03:05 +0800

    default category

--- src/app/controls/category/category-dropdown.component.ts
+++ src/app/controls/category/category-dropdown.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { CategoryService } from '../../services/category.service';
+import { IKeyValue } from '../../models/key-value.interface';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -6,6 +10,14 @@ import { Component } from '@angular/core';
   templateUrl: './category-dropdown.component.html'
 })
 
-export class CategoryDropdownComponent {
+export class CategoryDropdownComponent implements OnInit {
   isActive: boolean;
+  selectedCategory: BehaviorSubject<IKeyValue>;
+
+  constructor(private categoryService: CategoryService) {
+  }
+
+  ngOnInit() {
+    this.selectedCategory = this.categoryService.selectedCategory;
+  }
 }
