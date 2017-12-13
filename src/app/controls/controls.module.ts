ead38b0a4c1e49844f3711b40ee23462dce6b101
Vincent Shen <edentidus@foxmail.com>
2017-12-13 08:47:38 +0800

    move components

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -5,16 +5,12 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 import { EasyQuillEditorComponent } from './editor/ez-quill-editor.component';
-import { ContentPanelComponent } from './editor/content-panel.component';
-import { DetailPanelComponent } from './panel/detail-panel.component';
-import { DetailEditorComponent } from './editor/detail-editor.component';
-import { DetailViewComponent } from './view/detail-view.component';
+
 import { PaginationComponent } from './pagination.component';
 import { CategoryPanelComponent } from './category/category-panel.component';
 import { CategoryGroupItemComponent } from './category/category-group-item.component';
 import { CategoryDropdownComponent } from './category/category-dropdown.component';
 import { SearchBoxComponent } from './search-box.component';
-import { TagsEditorComponent } from './editor/tags-editor.component';
 import { NoContentComponent } from './no-content.component';
 import { ButtonGroupComponent } from './button-group.component';
 
@@ -26,30 +22,21 @@ import { ButtonGroupComponent } from './button-group.component';
   ],
   declarations: [
     EasyQuillEditorComponent,
-    ContentPanelComponent,
-    DetailPanelComponent,
-    DetailEditorComponent,
-    DetailViewComponent,
     PaginationComponent,
     CategoryPanelComponent,
     CategoryGroupItemComponent,
     CategoryDropdownComponent,
     SearchBoxComponent,
-    TagsEditorComponent,
     NoContentComponent,
     ButtonGroupComponent
   ],
   exports: [
     EasyQuillEditorComponent,
-    ContentPanelComponent,
-    DetailPanelComponent,
-    DetailEditorComponent,
     PaginationComponent,
     CategoryPanelComponent,
     CategoryGroupItemComponent,
     CategoryDropdownComponent,
     SearchBoxComponent,
-    TagsEditorComponent,
     NoContentComponent,
     ButtonGroupComponent
   ]
--- src/app/questions/questions.module.ts
+++ src/app/questions/questions.module.ts
@@ -8,6 +8,12 @@ import { QuestionsRoutingModule } from './questions-routing.module';
 import { QuestionService } from '../services/question.service';
 import { QuestionAnswerService } from '../services/question-answer.service';
 
+import { ContentPanelComponent } from './panel/content-panel.component';
+import { DetailPanelComponent } from './panel/detail-panel.component';
+import { DetailEditorComponent } from './editor/detail-editor.component';
+import { DetailViewComponent } from './view/detail-view.component';
+import { TagsEditorComponent } from './editor/tags-editor.component';
+
 import { QuestionListComponent } from './question-list.component';
 import { AskQuestionComponent } from './ask-question.component';
 import { QuestionDetailComponent } from './question-detail.component';
@@ -24,7 +30,12 @@ import { QuestionItemComponent } from './question-item.component';
     QuestionDetailComponent,
     QuestionListComponent,
     QuestionItemComponent,
-    AskQuestionComponent
+    AskQuestionComponent,
+    ContentPanelComponent,
+    DetailPanelComponent,
+    DetailEditorComponent,
+    DetailViewComponent,
+    TagsEditorComponent
   ],
   providers: [
     QuestionService,
