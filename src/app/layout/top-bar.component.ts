7edfceaf60d29d93790d986a03c3376ab2001c09
Vincent Shen <chengwei.shen@EF.com>
2017-12-22 13:28:35 +0800

    fix some ui issue

--- src/app/layout/top-bar.component.ts
+++ src/app/layout/top-bar.component.ts
@@ -1,7 +1,5 @@
 import {
-  Component, EventEmitter, Output,
-  AfterViewInit, Renderer, HostListener,
-  ViewChild, ElementRef
+  Component, EventEmitter, Output
 } from '@angular/core';
 import { Location } from '@angular/common';
 import { NavigationNode } from '../models/navigation.model';
@@ -12,18 +10,8 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   templateUrl: './top-bar.component.html'
 })
 
-export class TopBarComponent implements AfterViewInit {
-  shown = false;
-
-  public height: number;
-  public duration = 350;
-
-  public collapse = false;
-  public showClass = false;
-  public collapsing = false;
-
-  @ViewChild('collapseNav') el: ElementRef;
-  @ViewChild('nav') navbar: ElementRef;
+export class TopBarComponent {
+  isMenuActive = false;
 
   topMenuNodes: NavigationNode[] = [
     {
@@ -39,97 +27,15 @@ export class TopBarComponent implements AfterViewInit {
   ];
 
   constructor(private dialog: MatDialog,
-    private location: Location,
-    private renderer: Renderer) {
+    private location: Location) {
 
-  }
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.height = this.el.nativeElement.scrollHeight;
-      this.collapse = true;
-    });
   }
 
   doSearch(data: any) {
 
   }
 
-  toggle(event: any) {
-    event.preventDefault();
-    if (!this.collapsing) {
-      if (this.shown) {
-        this.hide();
-      } else {
-        this.show();
-      }
-    }
-  }
-
-  show() {
-    this.shown = true;
-    this.collapse = false;
-    this.collapsing = true;
-    setTimeout(() => {
-      this.renderer.setElementStyle(this.el.nativeElement, 'height', this.height + 'px');
-    }, 10);
-
-    setTimeout(() => {
-      this.collapse = true;
-      this.collapsing = false;
-      this.showClass = true;
-    }, this.duration);
-  }
-
-  hide() {
-    this.shown = false;
-    this.collapse = false;
-    this.showClass = false;
-    this.collapsing = true;
-    setTimeout(() => {
-      this.renderer.setElementStyle(this.el.nativeElement, 'height', '0px');
-    }, 10);
-
-    setTimeout(() => {
-      this.collapsing = false;
-      this.collapse = true;
-    }, this.duration);
-  }
-
-  get displayStyle() {
-    return '';
-  }
-
-  @HostListener('window:resize', ['$event']) onresize(event: any) {
-    const breakpoit = 992;
-
-    if (event.target.innerWidth < breakpoit) {
-      if (!this.shown) {
-        this.collapse = false;
-        this.renderer.setElementStyle(this.el.nativeElement, 'height', '0px');
-        this.renderer.setElementStyle(this.el.nativeElement, 'opacity', '0');
-        setTimeout(() => {
-          this.height = this.el.nativeElement.scrollHeight;
-          this.collapse = true;
-          this.renderer.setElementStyle(this.el.nativeElement, 'opacity', '');
-        }, 4);
-      }
-    } else {
-      this.collapsing = false;
-      this.shown = false;
-      this.showClass = false;
-      this.collapse = true;
-      this.renderer.setElementStyle(this.el.nativeElement, 'height', '');
-    }
-  }
-
-  @HostListener('document:scroll', ['$event']) onScroll() {
-    if (this.navbar.nativeElement.classList.contains('scrolling-navbar')) {
-      if (window.pageYOffset > 120) {
-        this.renderer.setElementClass(this.navbar.nativeElement, 'top-nav-collapse', true);
-      } else {
-        this.renderer.setElementClass(this.navbar.nativeElement, 'top-nav-collapse', false);
-      }
-    }
+  toggleMenu() {
+    this.isMenuActive = !this.isMenuActive;
   }
 }
