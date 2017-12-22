c48c239525ebadffd18e20758906333f5f110983
Vincent Shen <chengwei.shen@EF.com>
2017-12-22 16:13:54 +0800

    no env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  selfUrl: 'https://www.ysmbt.cn',
+  selfUrl: '',
   apiServerUrl: '',
   authSettings: {
-    tenantName: 'ysmbt.onmicrosoft.com',
-    clientId: 'a8c77bd9-5891-4889-9597-d652cfbdefb3',
-    scopes: ['https://ysmbt.onmicrosoft.com/api/read openid offline_access'],
-    policyName: 'B2C_1_ysmbt_sign',
+    tenantName: '',
+    clientId: '',
+    scopes: [''],
+    policyName: '',
   }
 };
